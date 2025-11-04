239925d9d8bf6ee3215f566f91bd9edde32a0a7c
alexisbridgewater <66692807+alexisbridgewater@users.noreply.github.com>
2025-11-04 11:42:45 -0400

    re-create Relationships

--- src/news_reporter/app.py
+++ src/news_reporter/app.py
@@ -14,7 +14,10 @@ async def main() -> None:
     cfg = Settings.load()
 
     # goal = "prepare a news script for John on latest news for the world?"
-    goal = "What are Alexis's skills you can find and his DXC Email address?"    
+    goal = "What are Kevin skills you can find and his DXC Email address?"    
+ #   goal = "tell me about Kevin ?"    
+
+
     print("Goal:", goal)
 
     result = await run_sequential_goal(cfg, goal)
